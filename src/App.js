cdc797ba9c0c22ef810c6abfe808c33a4d52e2b3
Karyna Selivanova <k.selivanova@aimprosoft.com>
2019-07-29 15:50:57 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from  './assets/styles/main.less'
+
+function App() {
+  return (
+    <div className="App" style={styles.app}>
+
+    </div>
+  );
+}
+
+export default App;
